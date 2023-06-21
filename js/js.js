a27e7898448419f0a9c957b4c278afd1f3eaa3b5
suhyeod <133014897+suhyeod@users.noreply.github.com>
2023-06-21 14:48:34 +0900

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,35 +1,48 @@
 const slides = document.querySelectorAll('.img_2 > ul '); //전체 슬라이드 컨테이너
 const slideImg_1 = document.querySelectorAll('.img_3_1'); //모든 슬라이드들 _ 1
-//const slideImg_2 = document.querySelectorAll('.img_4'); //모든 슬라이드들 _ 2
-let currentIdx = 0; //현재 슬라이드 index
+const slideImg_2 = document.querySelectorAll('.img_3_2'); //모든 슬라이드들 _ 2
+const slideImg_3 = document.querySelectorAll('.img_3_3'); //모든 슬라이드들 _ 3
+const slideImg_4 = document.querySelectorAll('.img_3_4'); //모든 슬라이드들 _ 4
+const slideImg_5 = document.querySelectorAll('.img_3_5'); //모든 슬라이드들 _ 5
+const slideImg_6 = document.querySelectorAll('.img_3_6'); //모든 슬라이드들 _ 6
+let currentIdx_1 = 0; //현재 슬라이드 index
+let currentIdx_2 = 0; //현재 슬라이드 index
+let currentIdx_3 = 0; //현재 슬라이드 index
+let currentIdx_4 = 0; //현재 슬라이드 index
+let currentIdx_5 = 0; //현재 슬라이드 index
+let currentIdx_6 = 0; //현재 슬라이드 index
 const slideCount_1 = slideImg_1.length; // 슬라이드 개수
-//const slideCount_2 = slideImg_2.length; // 슬라이드 개수
+const slideCount_2 = slideImg_2.length; // 슬라이드 개수
+const slideCount_3 = slideImg_3.length; // 슬라이드 개수
+const slideCount_4 = slideImg_4.length; // 슬라이드 개수
+const slideCount_5 = slideImg_5.length; // 슬라이드 개수
+const slideCount_6 = slideImg_6.length; // 슬라이드 개수
 const prev = document.querySelectorAll('.prev'); //이전 버튼
 const next = document.querySelectorAll('.next'); //다음 버튼
-const slideWidth = 219; //한개의 슬라이드 넓이
-const slideMargin = 80; //슬라이드간의 margin 값
 
-console.log(slideImg_1);
-//전체 슬라이드 컨테이너 넓이 설정
-slides[0].style.width = (slideWidth + slideMargin) * slideCount_1 + 'px';
-slides[1].style.width = (slideWidth + slideMargin) * slideCount_1 + 'px';
+console.log(slideCount_1, slideCount_2);
 
-//slides[2].style.width = (slideWidth + slideMargin) * slideCount_2 + 'px';
-//slides[3].style.width = (slideWidth + slideMargin) * slideCount_2 + 'px';
 function moveSlide(num, img_cont) {
     console.log(num, img_cont);
-    slides[img_cont].style.left = -num * (slideWidth + slideMargin) + 'px'; // 200px씩 이동
+    slides[img_cont].style.left = 'calc(' + (num*-12.5) + 'vw + ' + (num*-80) + 'px )'; 
     console.log(slides[img_cont].style.left);
-    currentIdx = num;
+    if(img_cont == 0) currentIdx_1 = num;
+    if(img_cont == 1) currentIdx_2 = num;
+    if(img_cont == 2) currentIdx_3 = num;
+    if(img_cont == 3) currentIdx_4 = num;
+    if(img_cont == 4) currentIdx_5 = num;
+    if(img_cont == 5) currentIdx_6 = num;
 }
 
+
+
+// 첫번째
 prev[0].addEventListener('click', function () {
     /*첫 번째 슬라이드로 표시 됐을때는
   이전 버튼 눌러도 아무런 반응 없게 하기 위해
   currentIdx !==0일때만 moveSlide 함수 불러옴*/
-
-    if (currentIdx !== 0)
-        moveSlide(currentIdx - 1, 0);
+    if (currentIdx_1 !== 0)
+        moveSlide(currentIdx_1 - 1, 0);
     }
 ); 
 
@@ -38,18 +51,21 @@ next[0].addEventListener('click', function () {
   다음 버튼 눌러도 아무런 반응 없게 하기 위해
   currentIdx !==slideCount - 1 일때만
   moveSlide 함수 불러옴 */
-    if (currentIdx !== slideCount_1 - 5) {
-        moveSlide(currentIdx + 1, 0);
+    if (currentIdx_1 !== slideCount_1 - 4) {
+        moveSlide(currentIdx_1 + 1, 0);
     }
 });
 
+
+
+//두번째
 prev[1].addEventListener('click', function () {
     /*첫 번째 슬라이드로 표시 됐을때는
   이전 버튼 눌러도 아무런 반응 없게 하기 위해
   currentIdx !==0일때만 moveSlide 함수 불러옴 */
 
-    if (currentIdx !== 0) 
-        moveSlide(currentIdx - 1, 1);
+    if (currentIdx_2 !== 0) 
+        moveSlide(currentIdx_2 - 1, 1);
     }
 );
 
@@ -58,72 +74,102 @@ next[1].addEventListener('click', function () {
   다음 버튼 눌러도 아무런 반응 없게 하기 위해
   currentIdx !==slideCount - 1 일때만
   moveSlide 함수 불러옴 */
-    if (currentIdx !== slideCount - 1) {
-        moveSlide(currentIdx + 1, 1);
+    if (currentIdx_2 !== slideCount_2 - 4) {
+        moveSlide(currentIdx_2 + 1, 1);
     }
 });
 
 
 
+//세번째
+prev[2].addEventListener('click', function () {
+    /*첫 번째 슬라이드로 표시 됐을때는
+  이전 버튼 눌러도 아무런 반응 없게 하기 위해
+  currentIdx !==0일때만 moveSlide 함수 불러옴 */
+
+    if (currentIdx_3 !== 0) 
+        moveSlide(currentIdx_3 - 1, 2);
+    }
+);
+
+next[2].addEventListener('click', function () {
+    /* 마지막 슬라이드로 표시 됐을때는
+  다음 버튼 눌러도 아무런 반응 없게 하기 위해
+  currentIdx !==slideCount - 1 일때만
+  moveSlide 함수 불러옴 */
+    if (currentIdx_3 !== slideCount_3 - 4) {
+        moveSlide(currentIdx_3 + 1, 2);
+    }
+});
 
 
 
+//네번째
+prev[3].addEventListener('click', function () {
+    /*첫 번째 슬라이드로 표시 됐을때는
+  이전 버튼 눌러도 아무런 반응 없게 하기 위해
+  currentIdx !==0일때만 moveSlide 함수 불러옴 */
+
+    if (currentIdx_4 !== 0) 
+        moveSlide(currentIdx_4 - 1, 3);
+    }
+);
+
+next[3].addEventListener('click', function () {
+    /* 마지막 슬라이드로 표시 됐을때는
+  다음 버튼 눌러도 아무런 반응 없게 하기 위해
+  currentIdx !==slideCount - 1 일때만
+  moveSlide 함수 불러옴 */
+    if (currentIdx_4 !== slideCount_4 - 4) {
+        moveSlide(currentIdx_4 + 1, 3);
+    }
+});
 
 
 
+//다섯번째
+prev[4].addEventListener('click', function () {
+    /*첫 번째 슬라이드로 표시 됐을때는
+  이전 버튼 눌러도 아무런 반응 없게 하기 위해
+  currentIdx !==0일때만 moveSlide 함수 불러옴 */
+
+    if (currentIdx_5 !== 0) 
+        moveSlide(currentIdx_5 - 1, 4);
+    }
+);
+
+next[4].addEventListener('click', function () {
+    /* 마지막 슬라이드로 표시 됐을때는
+  다음 버튼 눌러도 아무런 반응 없게 하기 위해
+  currentIdx !==slideCount - 1 일때만
+  moveSlide 함수 불러옴 */
+    if (currentIdx_5 !== slideCount_5 - 4) {
+        moveSlide(currentIdx_5 + 1, 4);
+    }
+});
 
 
 
+//여섯번째
+prev[5].addEventListener('click', function () {
+    /*첫 번째 슬라이드로 표시 됐을때는
+  이전 버튼 눌러도 아무런 반응 없게 하기 위해
+  currentIdx !==0일때만 moveSlide 함수 불러옴 */
 
+    if (currentIdx_6 !== 0) 
+        moveSlide(currentIdx_6 - 1, 5);
+    }
+);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+next[5].addEventListener('click', function () {
+    /* 마지막 슬라이드로 표시 됐을때는
+  다음 버튼 눌러도 아무런 반응 없게 하기 위해
+  currentIdx !==slideCount - 1 일때만
+  moveSlide 함수 불러옴 */
+    if (currentIdx_6 !== slideCount_6 - 4) {
+        moveSlide(currentIdx_6 + 1, 5);
+    }
+});
 
 
 
